e1f3ebbed62eadf02e6430e4afd8435c8e879788
Shyyn <shyynowo1@gmail.com>
2023-10-05 20:15:19 +0200

    e

--- main.py
+++ main.py
@@ -3,30 +3,32 @@ from random import *
 dead = 0
 hp1 = 100
 hp2 = 100
+J1 = input("P1 name: ")
+J2 = input("P2 name: ")
 
 while dead == 0:
     turn = randint(1, 2)
     input("Press enter to continue")
     if turn == 1:
-        print("P1 turn")
+        print(J1, "turn")
         att1 = randint(1, 15)
         print("DMG: ", att1)
         hp2 = hp2 - att1
-        print("HP P2: ", hp2)
+        print("HP", J2, ":", hp2)
         if hp2 <= 0:
             dead = 2
-            print("P2 is dead")
+            print(J2,"is dead")
     if turn == 2:
-            print("P2 turn")
+            print(J2,"turn")
             att2 = randint(1, 15)
             print("DMG: ", att2)
             hp1 = hp1 - att2
-            print("HP P1: ", hp1)
+            print("HP", J1, ":", hp1)
             if hp1 <= 0:
                 dead = 1
-                print("P1 is dead")
-                
+            print(J1,"is dead")
+
 if dead == 1:
-    print("P2 Won")
+    print(J2, "Won")
 elif dead == 2:
-    print("P1 Won")
+    print(J1, "Won")
