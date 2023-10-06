63e2ea166e237b3d6533dde759d36c26683eb7c4
Shyyn <shyynowo1@gmail.com>
2023-10-06 13:48:02 +0200

    stuff edited again

--- critical.py
+++ critical.py
@@ -1,6 +1,13 @@
 from random import *
-def crit(x):
-    y = att * 2
+from basile import att1, att2
+def crit1(x):
+    y = att1 * 2
+    return()
+
+def crit2(x):
+    w = att2 * 2
+    return()
+
 #att = 5
 #deg = att
 #crit = randint(1,20)
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from random import *
+
+def crit1(x):
+    y = att1 * 2 + 10
+    return()
+
+def crit2(x):
+    w = att2 * 2 + 10
+    return(w)
+
 dead = 0
 hp1 = 100
 hp2 = 100
@@ -10,9 +19,14 @@ while dead == 0:
     if turn == 1:
         print(J1, "turn")
         att1 = randint(1, 15)
-        print("DMG: ", att1)
-        hp2 = hp2 - att1
-        print("HP", J2, ":", hp2)
+        critical_dmg1 = crit1(att1)
+        p_crit = randint(1, 45)
+        if p_crit == 14:
+             print(critical_dmg2, "coup critique dans ta mère")
+        else:
+            print("DMG: ", att1)
+            hp2 = hp2 - att1
+            print("HP", J2, ":", hp2)
     elif hp1 <= 0:
          dead = 1
          print(J1, "is dead")
@@ -24,8 +38,13 @@ while dead == 0:
        print(J1, "won")
        break
     if turn == 2:
-            print(J2,"turn")
-            att2 = randint(1, 15)
+        print(J2,"turn")
+        att2 = randint(1, 15)
+        critical_dmg2 = crit2(att2)
+        p_crit = randint(1, 45)
+        if p_crit == 14:
+             print(critical_dmg2, "coup critique dans ta mère")
+        else:
             print("DMG: ", att2)
             hp1 = hp1 - att2
             print("HP", J1, ":", hp1)
