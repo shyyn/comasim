888a8749a1973288e6976a4091a0ec8f0a596e14
Shyyn <shyynowo1@gmail.com>
2023-10-06 13:04:09 +0200

    stuff edited

---
+++ critical.py
@@ -0,0 +1,16 @@
+from random import *
+def crit(x):
+    y = att * 2
+#att = 5
+#deg = att
+#crit = randint(1,20)
+#if crit == 14:
+ #   deg = att * 2
+   # print(deg, "Coup critique dans ta mere")
+#else:
+ #   print(att)
+
+
+
+
+    
--- test/eliott.py
+++ test/eliott.py
@@ -1,4 +1,5 @@
 from random import *
+
 dead = 0
 hp1 = 100
 hp2 = 100
@@ -13,29 +14,19 @@ while dead == 0:
         print("DMG: ", att1)
         hp2 = hp2 - att1
         print("HP", J2, ":", hp2)
-    elif hp1 <= 0:
-         dead = 1
-         print(J1, "is dead")
-         print(J2, "won")
-         break
     elif hp2 <= 0:
        dead = 2
        print(J2, "is dead")
        print(J1, "won")
-       break
+       break   
     if turn == 2:
             print(J2,"turn")
             att2 = randint(1, 15)
             print("DMG: ", att2)
             hp1 = hp1 - att2
             print("HP", J1, ":", hp1)
-    elif hp2 <= 0:
-        dead = 1
-        print(J2, "is dead")
-        print(J1, "won")
-        break
     elif hp1 <= 0:
-         dead = 1
-         print(J1, "is dead")
-         print(J2, "won")
-         break
+       dead = 1
+       print(J1,"is dead")
+       print(J2, "Won")
+       break
